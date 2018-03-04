eacf84c40babf6c87c6f53cef13f784ce464c288
David Hong <davidhong342@gmail.com>
2018-03-03 18:39:07 -0800

    d3 integration

---
+++ src/components/stock-chart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Chart from './chart/CandleStickChartForContinuousIntraDay';
+import { getData } from "../utils"
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
+
+
+export default class StockChart extends React.Component {
+	componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+	}
+	render() {
+		if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		return (
+			<TypeChooser>
+				{type => <Chart type={type} data={this.state.data} />}
+			</TypeChooser>
+		)
+	}
+}
